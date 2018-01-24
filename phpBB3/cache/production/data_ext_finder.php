ad2150b921e74db4a0964450226f4ffaad115fc0
unknown <motoparsing>
2018-01-24 23:06:42 +0300

    Создание phpBB на локалке

---
+++ phpBB3/cache/production/data_ext_finder.php
@@ -0,0 +1,4 @@
+<?php exit; ?>
+1548356334
+922
+a:3:{s:32:"d02806c98648acbc3e4c04be69a4e244";a:1:{i:0;a:4:{s:10:"named_path";s:50:"ext/phpbb/viglink/language/en/info_acp_viglink.php";s:8:"ext_name";s:13:"phpbb/viglink";s:4:"path";s:30:"ext/phpbb/viglink/language/en/";s:8:"filename";s:20:"info_acp_viglink.php";}}s:32:"9edf501a5755e0575c3e210c8f505815";a:1:{i:0;a:4:{s:10:"named_path";s:50:"ext/phpbb/viglink/language/ru/info_acp_viglink.php";s:8:"ext_name";s:13:"phpbb/viglink";s:4:"path";s:30:"ext/phpbb/viglink/language/ru/";s:8:"filename";s:20:"info_acp_viglink.php";}}s:32:"4ebd2ba8c55128ea8e0cdeab003a10b2";a:2:{i:0;a:4:{s:10:"named_path";s:37:"language/en/acp/permissions_phpbb.php";s:8:"ext_name";s:1:"/";s:4:"path";s:16:"language/en/acp/";s:8:"filename";s:21:"permissions_phpbb.php";}i:1;a:4:{s:10:"named_path";s:37:"language/ru/acp/permissions_phpbb.php";s:8:"ext_name";s:1:"/";s:4:"path";s:16:"language/ru/acp/";s:8:"filename";s:21:"permissions_phpbb.php";}}}
